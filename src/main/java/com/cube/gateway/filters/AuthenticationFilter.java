ed87acdc7a76194f5dc770538822a5311bbd3978
Matheus Júnior <matheusfpjunior@gmail.com>
2025-09-03 22:14:29 -0300

    Revert "refactor: add header to skip ngrok warning in every request"
    This reverts commit c4af13e0c48e39f2b000e923f6943563bdd8a898.

--- src/main/java/com/cube/gateway/filters/AuthenticationFilter.java
+++ src/main/java/com/cube/gateway/filters/AuthenticationFilter.java
@@ -98,6 +98,7 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
                             .request(builder -> builder
                                     .header("customer_id", username)
                                     .header("asaas_customer_id", asaasId)
+                                    .header("ngrok-skip-browser-warning", "69420")
                             )
                             .build();
 
@@ -112,12 +113,6 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
                 }
 
             }
-
-            exchange = exchange.mutate()
-                    .request(builder -> builder
-                            .header("ngrok-skip-browser-warning", "69420")
-                    )
-                    .build();
 
             log.info("Gateway filter end");
 
